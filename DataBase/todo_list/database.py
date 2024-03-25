548c2493db913e2b175a58dcd49c62dcf2aa0b24
MalrangCow <malrangcow00@gmail.com>
2024-03-25 18:00:34 +0900

    0325_TIL

--- DataBase/todo_list/database.py
+++ DataBase/todo_list/database.py
@@ -1,1 +1,11 @@
-SQLALCHEMY_DATABASE_URL = 'jdbc:mysql://localhost:3306/fastapi'
+# SQLALCHEMY_DATABASE_URL = 'jdbc:mysql://localhost:3306/fastapi'
+
+# database.py
+from sqlalchemy import create_engine, MetaData
+from databases import Database
+from .config import DATABASE_URL
+
+engine = create_engine(DATABASE_URL)
+metadata = MetaData()
+
+database = Database(DATABASE_URL)
